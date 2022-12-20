5ffda7fc4edad711f2413a941a96f9bec4a2909f
cowlsla <cowlsla@naver.com>
2022-12-20 15:16:52 +0900

    2022-12-20

--- syntax/outputs.tf
+++ syntax/outputs.tf
@@ -1,3 +1,11 @@
-output "number_example" {
-  value = var.number
+output "number_output" {
+  value = var.number_example
+}
+
+output "string_output" {
+  value = var.string_example
+}
+
+output "list_output" {
+  value = var.list_example 
 }
--- syntax/variables.tf
+++ syntax/variables.tf
@@ -1,6 +1,14 @@
-variable "number"{
-	description = "An example of a number variable in Terraform"
-	type = number
-	default = 422
+variable "number_example" {
+  type        = number
+  default     = 100
 }
 
+variable "string_example" {
+  type        = string
+  default     = "terraform-example-instance"
+}
+
+variable "list_example" {
+  type        = list(string)
+  default     = ["a", "b", "c"]
+}
